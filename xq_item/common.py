7379f3997f2fac62f43351fca5e3a1b60b816b99
Zhou, Billy <billy.zhou@aptiv.com>
2025-09-14 11:33:15 +0800

    Add get_basic_info()
    - Add get_basic_info()
    - Move common functions to common.py

---
+++ xq_item/common.py
@@ -0,0 +1,59 @@
+import json
+
+from datetime import datetime
+
+import requests
+
+
+def read_token() -> str:
+    """
+    Read the token from the token file.
+
+    This method reads the token from the token file and returns it.
+
+    Returns:
+        str: The token read from the token file.
+    """
+    with open('xq_item\\token.txt', 'r') as f:
+        return f.read().strip()
+
+
+def get_http_response(token, url):
+    """
+    Get HTTP response from the specified URL.
+
+    This method sends a GET request to the specified URL and returns the response.
+
+    Parameters:
+    - url (str): The URL to which the request is sent.
+
+    Returns:
+    - requests.Response: The HTTP response from the specified URL.
+    """
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0',
+        'Cookie': token,
+        'Content-Type': 'application/json'
+    }
+
+    r = requests.get(url, headers=headers)
+
+    return json.loads(r.text)
+
+
+def conv_timestamp(ts):
+    """
+    Convert timestamp to datetime object.
+
+    This function converts a Unix timestamp in milliseconds to a datetime object.
+    The input timestamp is divided by 1000 because the input is in milliseconds
+    while the datetime.fromtimestamp() function expects seconds.
+
+    Parameters:
+    - ts (int or float): Unix timestamp in milliseconds
+
+    Returns:
+    - datetime: A datetime object representing the timestamp
+    """
+    t = datetime.fromtimestamp(ts / 1000)
+    return t
--- xq_item/cube.py
+++ xq_item/cube.py
@@ -1,64 +1,11 @@
 import json
 import logging
 import re
+import time
 
 from datetime import datetime
 
-import requests
-
-
-def read_token() -> str:
-    """
-    Read the token from the token file.
-
-    This method reads the token from the token file and returns it.
-
-    Returns:
-        str: The token read from the token file.
-    """
-    with open('xq_item\\token.txt', 'r') as f:
-        return f.read().strip()
-
-
-def get_http_response(token, url):
-    """
-    Get HTTP response from the specified URL.
-
-    This method sends a GET request to the specified URL and returns the response.
-
-    Parameters:
-    - url (str): The URL to which the request is sent.
-
-    Returns:
-    - requests.Response: The HTTP response from the specified URL.
-    """
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0',
-        'Cookie': token,
-        'Content-Type': 'application/json'
-    }
-
-    r = requests.get(url, headers=headers)
-
-    return json.loads(r.text)
-
-
-def conv_timestamp(ts):
-    """
-    Convert timestamp to datetime object.
-    
-    This function converts a Unix timestamp in milliseconds to a datetime object.
-    The input timestamp is divided by 1000 because the input is in milliseconds
-    while the datetime.fromtimestamp() function expects seconds.
-    
-    Parameters:
-    - ts (int or float): Unix timestamp in milliseconds
-    
-    Returns:
-    - datetime: A datetime object representing the timestamp
-    """
-    t = datetime.fromtimestamp(ts / 1000)
-    return t
+from xq_item import common
 
 
 class Cube:
@@ -80,7 +27,7 @@ class Cube:
         # Convert the input cube_id to uppercase and assign it to the instance attribute to ensure ID uniformity and case insensitivity
         self.cube_id = cube_id.upper()
 
-        self.token = read_token()
+        self.token = common.read_token()
 
         # Build the cube location URL based on cube_id for subsequent access or reference to the cube's location information
         self.position_url = self.CUBE_POSITION_URL + self.cube_id
@@ -112,7 +59,7 @@ class Cube:
             self.logger.error("Cube ID is in invalid format.")
             return 1
 
-        r = get_http_response(self.token, self.alldata_url)
+        r = common.get_http_response(self.token, self.alldata_url)
         # print(r)
 
         # check type of r
@@ -140,6 +87,34 @@ class Cube:
                     self.logger.info("Cube exists.")
                     return 0
 
+    def get_basic_info(self) -> dict:
+        """
+        Get basic information of the cube.
+        
+        This method retrieves the basic information of a cube including its name,
+        creation date, and current value by making an HTTP request to the all data URL.
+        It parses the response to extract these key information elements.
+        
+        Returns:
+            dict: A dictionary containing the following keys:
+                  - 'name': The name of the cube
+                  - 'created_on': The creation date of the cube in 'YYYY-MM-DD' format
+                  - 'value': The latest value of the cube
+        """
+        info = {}
+
+        r = {}
+        while type(r) == dict:
+            # If the response is dict, it means the get failed, so we need to retry
+            r = common.get_http_response(self.token, self.alldata_url)
+            self.logger.info(f"Get cube info failed. Retrying...")
+            time.sleep(3)
+        info['name'] = r[0]['name']
+        info['created_on'] = r[0]['list'][0]['date']
+        info['value'] = r[0]['list'][-1]['value']
+
+        return info
+
     def get_rebalance(self):
         """
         Get rebalancing history of the cube.
@@ -151,14 +126,15 @@ class Cube:
             list: A list of rebalancing records if successful, None if failed to get data.
                   Each record contains details about a specific rebalancing event.
         """
-        r = get_http_response(self.token, self.rebalance_url)
+        r = common.get_http_response(self.token, self.rebalance_url)
         try:
             rebalance = r['list']
             return rebalance
         except json.JSONDecodeError:
             self.logger.error("Failed to get cube rebalances.")
+            return None
 
-    def get_specific_day_rebalance(self, date):
+    def get_specific_day_rebalance(self, date) -> list:
         """
         Get specific day's successful user-initiated rebalancing records.
         
@@ -184,7 +160,7 @@ class Cube:
 
         if rebalance:
             for item in rebalance:
-                rebalance_date = conv_timestamp(item['updated_at']).strftime('%Y%m%d')
+                rebalance_date = common.conv_timestamp(item['updated_at']).strftime('%Y%m%d')
                 if date == rebalance_date:
                     specific_day_rebalance.append(item)
         return specific_day_rebalance
